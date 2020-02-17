1a6682816b9ce774c26a5f1f8a7c16738326f51d
Jessica Li <jli6@andrew.cmu.edu>
2020-02-17 12:11:43 -0500

    Changed the Article and Category Models a little

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
 	has_many :photos
-	has_many :articles
+	has_one :article
 
 	scope :active, 	     -> { where('active = ?', true) }
   	scope :alphabetical, -> { order('name') }
