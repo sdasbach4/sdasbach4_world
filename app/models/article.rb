1d8949bcc2432c89ecced7af98eb5dcff0e965e0
Jessica Li <jli6@andrew.cmu.edu>
2020-02-17 12:12:54 -0500

    Misspelled presence

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ApplicationRecord
     has_one :category
 
-    validates_presense_of :title
-    validates_presense_of :content
+    validates_presence_of :title
+    validates_presence_of :content
 
     scope :active, 	     -> { where('active = ?', true) }
     scope :alphabetical, -> { order('title') }
